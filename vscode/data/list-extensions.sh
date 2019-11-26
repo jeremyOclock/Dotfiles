bbaead9756b9fc52934ba84a667e52756da23d28
jeremyOclock <jere.bs92@gmail.com>
2019-11-26 12:39:13 +0100

    updating vscode config

--- vscode/data/list-extensions.sh
+++ vscode/data/list-extensions.sh
@@ -1,3 +1,4 @@
+code --install-extension mrmlnc.vscode-duplicate
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension christian-kohler.path-intellisense
 code --install-extension CoenraadS.bracket-pair-colorizer-2
@@ -9,15 +10,16 @@ code --install-extension eg2.vscode-npm-script
 code --install-extension esbenp.prettier-vscode
 code --install-extension FallenMax.mithril-emmet
 code --install-extension Gajesh1.React-JS-Snippet
-code --install-extension johnpapa.Angular2
 code --install-extension mgmcdermott.vscode-language-babel
 code --install-extension mikestead.dotenv
 code --install-extension mrmlnc.vscode-duplicate
 code --install-extension ms-vscode.atom-keybindings
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension PeterJausovec.vscode-docker
 code --install-extension PKief.material-icon-theme
 code --install-extension ritwickdey.LiveServer
+code --install-extension robinbentley.sass-indented
 code --install-extension Shan.code-settings-sync
 code --install-extension suming.react-proptypes-generate
 code --install-extension WallabyJs.quokka-vscode
